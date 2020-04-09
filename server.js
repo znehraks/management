1cd570138db9fe1e39efece141f382c7c6e2ba2f
DesignC <znehraks@gmail.com>
2020-04-09 17:56:28 +0900

    Add Get Customers REST API Module

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "홍길동",
+      birthday: "960212",
+      gender: "남자",
+      job: "대학생"
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "동길동",
+      birthday: "961122",
+      gender: "남자",
+      job: "공대생"
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "롱길동",
+      birthday: "950622",
+      gender: "여자",
+      job: "대학생"
+    }
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
